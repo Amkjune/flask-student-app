55839b42596a1212dc1f40827ff09055eeb3805e
Amkjune <ammykour76@gmail.com>
2025-05-26 23:59:27 -0400

    database disconnected

--- app.py
+++ app.py
@@ -4,21 +4,10 @@ from mysql.connector import connection
 
 app = Flask(__name__)
 
-# MySQL connection
-cnx = connection.MySQLConnection(user='root', password='Ammy@123', host='host.docker.internal', database='mydb')
-cursor = cnx.cursor()
+
 
 # Create table if not exists
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Student_records (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    University VARCHAR(100),
-    Student_name VARCHAR(100),
-    Roll_number VARCHAR(100),
-    Courses TEXT
-)
-""")
-cnx.commit()
+
 
 @app.route('/', methods=['GET', 'POST'])
 def student_form():
@@ -28,10 +17,10 @@ def student_form():
         courses = request.form.getlist('courses')  # list of selected courses
         course_str = ", ".join(courses)
 
-        sql = "INSERT INTO Student_records (University, Student_name, Roll_number, Courses) VALUES (%s, %s, %s, %s)"
-        val = ('LPU', name, roll, course_str)
-        cursor.execute(sql, val)
-        cnx.commit()
+       # sql = "INSERT INTO Student_records (University, Student_name, Roll_number, Courses) VALUES (%s, %s, %s, %s)"
+        #val = ('LPU', name, roll, course_str)
+        #cursor.execute(sql, val)
+       # cnx.commit()
 
         return redirect('/students')
 
@@ -39,9 +28,10 @@ def student_form():
 
 @app.route('/students')
 def show_students():
-    cursor.execute("SELECT * FROM Student_records")
-    records = cursor.fetchall()
-    return f"<h2>Students:</h2>" + "<br>".join(str(row) for row in records)
+    print("hello")
+    #cursor.execute("SELECT * FROM Student_records")
+    #records = cursor.fetchall()
+    #return f"<h2>Students:</h2>" + "<br>".join(str(row) for row in records)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
